96014597c727dfa0a6f84542f1232db42ee1ab89
ashusoni24 <24ashusoni@gmail.com>
2025-10-02 01:39:32 +0530

    Chat models

---
+++ 2.ChatModels/4_chatmodel_huggingfaceapi.py
@@ -0,0 +1,12 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+llm = HuggingFaceEndpoint(
+    repo_id = "HuggingFaceTB/SmolLM3-3B",
+    task = "text-generation"
+)
+model = ChatHuggingFace(llm = llm)
+
+result = model.invoke("What is the capital of india")
+print(result.content)
